ed41872623e4b735037070a2f36f15011e028fa7
Lee Borkman <leeborkman@gmail.com>
2019-07-06 23:47:52 +1000

    Adding Port and IP config for OpenShift

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,12 @@ const app = express()
 const router = require('./routes')
 app.use('/', router)
 
-const port = 8000
-app.listen(port, () => {
-  console.log('We are live on ' + port)
-})
+
+// Setting up port and IP to run on OpenShift or locally
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
