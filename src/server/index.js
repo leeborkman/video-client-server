7ddb565420e1d984449a79bb178f8da4665d3468
Lee Borkman <leeborkman@gmail.com>
2019-07-07 00:55:18 +1000

    semi-colons

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,15 @@
-const express = require('express')
+const express = require('express');
+const router = require('./routes');
 
-const app = express()
-
-const router = require('./routes')
-
-app.use('/', router)
+const app = express();
+app.use('/', router);
 
 
 // Setting up port and IP to run on OpenShift or locally
-const serverPort = process.env.OPENSHIFT_NODEJS_PORT || 8080
-const serverIP = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+const serverPort = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const serverIP = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
  
 app.listen(serverPort, serverIP, () => {
-  console.log( `Listening on ${  serverIP  }, port ${  serverPort}` ) //eslint-disable-line
+  console.log( `Listening on ${  serverIP  }, port ${  serverPort}` ); //eslint-disable-line
 });
 
