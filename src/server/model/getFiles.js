9f229c2a93e7e283ac1a5a57486614849dd2c1d5
Lee Borkman <leeborkman@gmail.com>
2019-07-07 09:50:28 +1000

    Linting

--- src/server/model/getFiles.js
+++ src/server/model/getFiles.js
@@ -3,9 +3,10 @@ const fs = require('fs');
 function getFiles(callback) {
     fs.readdir("uploads", (err, items) => {
         if (err) {
-            return console.log('Unable to read uploads directory: ', err);
+            return console.log('Unable to read uploads directory: ', err); //eslint-disable-line
         }     
         callback(items);
+        return true;
     });
 }
 
--- src/server/routes.js
+++ src/server/routes.js
@@ -5,20 +5,20 @@ const uploadForm = require('./view/uploadForm');
 const router = app.Router();
 
 // Only three ways through this server app
+
+// A simple REST File List
 router.get('/', async (req, res) => {
-  const result = getFiles( (data) => { res.send({ files: data } ); });
+  getFiles( (data) => { res.send({ files: data } ); });
 });
 
+// A development form for testing basic upload function 
 router.get('/form', (req, res) => {
   res.send(uploadForm);
 });
 
+// Upload handle
 router.post('/upload', (req, res) => {
   res.send('uploads a video and returns the resulting list of videos');
-});
-
-router.get('/upload', (req, res) => {
-  res.send('uploads a video and returns the resulting list of videos - BUT SHOULD BE POST');
 });
 
 module.exports = router;
