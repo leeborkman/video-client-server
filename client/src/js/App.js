e9dbb4ad8bd136c1b5ff43b7652527810a223e9d
Lee Borkman <leeborkman@gmail.com>
2019-07-07 23:07:14 +1000

    restructuring repo

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,16 +1,7 @@
 import React from 'react';
+import config from './config/config';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import VideoGrid from './view/VideoGrid';
-
-import config from 'react-global-configuration';
-
-config.set({ 
-  videoServerRemote: 'http://vid-col-video-collection.apps.us-east-2.online-starter.openshift.com',
-  videoServer: '//localhost:8080',
-  api: '/api',
-  upload: '/upload'
-});
-
 
 
 
@@ -18,19 +9,6 @@ config.set({
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload   YAY.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <VideoGrid/>
     </div>
   );
---
+++ client/src/js/config/config.js
@@ -0,0 +1,9 @@
+import config from 'react-global-configuration';
+
+config.set({ 
+  videoServer: 'http://vid-col-video-collection.apps.us-east-2.online-starter.openshift.com',
+  videoServerLocal: '//localhost:8080',
+  api: '/api',
+  upload: '/upload'
+});
+
