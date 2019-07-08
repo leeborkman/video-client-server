eeaa3c324498863296d64a040f51cf9d0ea694aa
Lee Borkman <leeborkman@gmail.com>
2019-07-08 23:04:33 +1000

    restore eslint for react

--- client/src/js/App.js
+++ client/src/js/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import config from './config/config'
 import VideoGrid from './view/VideoGrid'
 
--- client/src/js/view/VideoGrid.js
+++ client/src/js/view/VideoGrid.js
@@ -1,37 +1,51 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
+import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 
 import GetVideoList from '../model/GetVideoList'
 
-export default class VideoGrid extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      data: { files: [] }
-    }
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    width: '100%'
+  },
+  grid: {
+    width: '100%',
+    margin: 'auto',
+    marginTop: '2em'
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    color: theme.palette.text.primary,
+    backgroundColor: theme.palette.primary.main
   }
+}))
 
-  componentDidMount () {
+export default function VideoGrid () {
+  const classes = useStyles()
+  const [files, setFiles] = useState([])
+
+  useEffect(() => {
     GetVideoList(data => {
-      this.setState({ data: data })
+      setFiles(data.files)
     })
-  }
+  }, [])
 
-  render () {
-    return (
-      <div>
-        <Grid container spacing={3}>
-          {this.state.data.files.map((file, i) => (
-            <Grid item xl={2} lg={3} md={4} sm={6} xs={12} key={i}>
-              <Card>{file.filename}</Card>
-            </Grid>
-          ))}
-        </Grid>
-      </div>
-    )
-  }
+  return (
+    <div className={classes.root}>
+      <Grid container justify='center' spacing={3} className={classes.grid}>
+        {files.map((file) => (
+          <Grid item xl={2} lg={3} md={4} sm={6} xs={12} key={file.filename}>
+            <Paper className={classes.paper}>{file.filename}</Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  )
 }
