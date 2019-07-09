53c4430cebbc214bc141be2f27675af6af0deef9
Lee Borkman <lee.borkman@transport.nsw.gov.au>
2019-07-09 13:42:45 +1000

    file upload with dropzone

--- client/src/js/view/UploadForm.js
+++ client/src/js/view/UploadForm.js
@@ -1,60 +1,64 @@
-/* eslint-disable react/jsx-filename-extension */
-import React, { useState, useEffect } from 'react'
-
+import React from 'react';
+import Dropzone from 'react-dropzone-uploader'
 import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
-
 import config from 'react-global-configuration'
-import ReactPlayer from 'react-player'
-import GetVideoList from '../model/GetVideoList'
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    width: '100%'
-  },
-  grid: {
-    width: '100%',
-    margin: 'auto',
-    marginTop: '2em'
-  },
   paper: {
+    margin: '2em auto 2em auto',
+    marginBottom: '4em',
     padding: theme.spacing(2),
     textAlign: 'center',
     overflow: 'hidden',
     textOverflow: 'ellipsis',
     color: theme.palette.text.primary,
-    backgroundColor: theme.palette.primary.main
+    backgroundColor: theme.palette.secondary.light
+  },
+  dropzone: {
+    border: '1px solid black',
+    width: '90%',
+    margin: '2em auto 2em auto',
+    padding: '2em',
+    textAlign: 'center'
+
   }
 }))
 
-export default function VideoGrid () {
+export default function UploadForm() {
   const classes = useStyles()
-  const [files, setFiles] = useState([])
 
-  useEffect(() => {
-    GetVideoList(data => {
-      setFiles(data.files)
-    })
-  }, [])
+  const getUploadParams = ({ meta }) => {
+    return { url: `${config.get('videoServer')}${config.get('uploadHandler')}` }
+  }
+
+  const onSubmit = (files) => {
+    console.log(files.map(f => f.meta))
+  }
+
+  const onChangeStatus = ({ meta, file }, status) => {
+    console.log(status, meta, file)
+  }
 
   return (
-    <div className={classes.root}>
-      <Grid container justify='center' spacing={3} className={classes.grid}>
-        {files.map(file => (
-          <Grid item xl={2} lg={3} md={4} sm={6} xs={12} key={file.filename}>
-            <Paper className={classes.paper}>
-              <ReactPlayer
-                url={`${config.get('videoServer')}/${file.filename}`}
-                width='100%'
-                height='100%'
-                controls
-              />
-            </Paper>
-          </Grid>
-        ))}
+    <Grid container justify='center' spacing={3} className={classes.grid}>
+      <Grid item xl={4} lg={4} md={6} sm={6} xs={11}>
+        <Paper className={classes.paper}>
+          <div className={classes.dropzone}>
+            <Dropzone
+              getUploadParams={getUploadParams}
+              onChangeStatus={onChangeStatus}
+              onSubmit={onSubmit}
+              accept="video/*"
+            />
+          </div>
+        </Paper>
       </Grid>
-    </div>
+    </Grid>
   )
 }
+
+
+
+
