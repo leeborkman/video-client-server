63a805a111580330c86fc36046053cebc2b0de63
Lee Borkman <leeborkman@gmail.com>
2019-07-16 00:00:28 +1000

    Remove debug code

--- client/src/js/model/getVideoList.js
+++ client/src/js/model/getVideoList.js
@@ -3,7 +3,6 @@ import config from 'react-global-configuration';
 export default async function getVideoList () {
   const api = `${config.get('videoServer')}${config.get('api')}`;
   const response = await fetch(api);
-  console.log('RESPONSE', response);
   const json = await response.json();
   return json;
 }
