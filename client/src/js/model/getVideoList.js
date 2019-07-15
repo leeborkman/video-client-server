e7af72a830bcf93a2030d5368d85a3c219f436d0
Lee Borkman <leeborkman@gmail.com>
2019-07-15 23:57:02 +1000

    Linting semi-colons

--- client/src/js/model/getVideoList.js
+++ client/src/js/model/getVideoList.js
@@ -3,7 +3,7 @@ import config from 'react-global-configuration';
 export default async function getVideoList () {
   const api = `${config.get('videoServer')}${config.get('api')}`;
   const response = await fetch(api);
-  console.log("RESPONSE", response)
+  console.log('RESPONSE', response);
   const json = await response.json();
   return json;
 }
--- client/src/js/model/getVideoList.test.js
+++ client/src/js/model/getVideoList.test.js
@@ -1,25 +1,25 @@
-import '@testing-library/jest-dom/extend-expect'
-import setConfigs from '../config/config'
-import getVideoList from './getVideoList'
+import '@testing-library/jest-dom/extend-expect';
+import setConfigs from '../config/config';
+import getVideoList from './getVideoList';
 
-setConfigs()
+setConfigs();
 
 test('data.files is an array', done => {
   getVideoList().then(data => {
     if (Array.isArray(data.files)) {
-      done()
+      done();
     } else {
-      done.fail('Was not an array')
+      done.fail('Was not an array');
     }
-  })
-})
+  });
+});
 
 test('data.files has at least one element', done => {
   getVideoList().then(data => {
     if (data.files.length > 0) {
-      done()
+      done();
     } else {
-      done.fail('Array was zero length')
+      done.fail('Array was zero length');
     }
-  })
-})
+  });
+});
