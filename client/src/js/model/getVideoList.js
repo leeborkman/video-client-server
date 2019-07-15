9bd7472f446ac47950368dadc7e56f1cb7b48b29
Lee Borkman <leeborkman@gmail.com>
2019-07-15 23:26:56 +1000

    Replacing string concat with backticks

--- client/src/js/model/getVideoList.js
+++ client/src/js/model/getVideoList.js
@@ -1,7 +1,7 @@
 import config from 'react-global-configuration'
 
 export default async function getVideoList (callback) {
-  const api = config.get('videoServer') + config.get('api')
+  const api = `${config.get('videoServer')}${config.get('api')}`
   fetch(api)
     .then(response => response.json())
     .then(json => {
