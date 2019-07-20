108fdf84c7e627859c7b50e5b29be266d8b35581
Lee Borkman <leeborkman@gmail.com>
2019-07-20 23:56:28 +1000

    Temporarily silence warning about act()

--- client/src/js/view/VideoGrid.test.js
+++ client/src/js/view/VideoGrid.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { render, fireEvent, cleanup, waitForElement } from '@testing-library/react'
+import { render, fireEvent, cleanup, waitForElement, wait, act } from '@testing-library/react'
+//import { act } from 'react-dom/test-utils';
 
 import '@testing-library/jest-dom/extend-expect'
 
@@ -12,17 +13,29 @@ afterEach(cleanup)
 
 jest.mock('../model/getVideoList')
 
+// WORKAROUND TO SILENCE WARNINGS ABOUT WRAPPING IN ACT() - USE WITH CAUTION
+const originalError = console.error;
+beforeAll(() => {
+  console.error = jest.fn();
+});
+afterAll(() => {
+  console.error = originalError;
+});
+
+
 setConfigs()
 
 test('loads and displays one VideoGrid', async () => {
+  act(() => { 
   const { getByText, getByTestId, queryAllByTestId, container, asFragment } = render(<VideoGrid />)
-  const videoGridNode = await waitForElement(() => getByTestId('video-grid'))
+  const videoGridNode = getByTestId('video-grid')
   expect(queryAllByTestId('video-grid')).toHaveLength(1)
+  })
 })
 
 test('displays at least one ReactPlayer', async () => {
   const { getByText, getByTestId, queryAllByTestId, container, asFragment } = render(<VideoGrid />)
-  const videoGridNode = await waitForElement(() => getByTestId('video-grid'))
+  const videoGridNode =  await waitForElement(() => getByTestId('video-grid'))
   expect(queryAllByTestId('react-player').length).toBeGreaterThan(0)
 })
 
