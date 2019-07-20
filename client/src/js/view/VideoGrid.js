d2aa03f9f6e26d3983e52a40c9a8a4a23a165c39
Lee Borkman <leeborkman@gmail.com>
2019-07-20 17:16:43 +1000

    VideoGrid updated so displays only *video* filested so now - passes all test now

--- client/src/js/view/VideoGrid.js
+++ client/src/js/view/VideoGrid.js
@@ -1,13 +1,13 @@
 /* eslint-disable react/jsx-filename-extension */
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import filesize from 'filesize';
+import React, { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import filesize from 'filesize'
 
-import config from 'react-global-configuration';
-import ReactPlayer from 'react-player';
-import getVideoList from '../model/getVideoList';
+import config from 'react-global-configuration'
+import ReactPlayer from 'react-player'
+import getVideoList from '../model/getVideoList'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -34,40 +34,48 @@ const useStyles = makeStyles(theme => ({
   filesize: {
     color: '#00f'
   }
-}));
+}))
 
 export default function VideoGrid () {
-  const classes = useStyles();
-  const [files, setFiles] = useState([]);
+  const classes = useStyles()
+  const [files, setFiles] = useState([])
 
   useEffect(() => {
     getVideoList().then(data => {
-      setFiles(data.files);
-    });
-  }, []);
+      setFiles(data.files)
+    })
+  }, [])
 
   return (
     <div className={classes.root} data-testid='video-grid'>
       <Grid container justify='center' spacing={3} className={classes.grid} data-testid='grid-top'>
         {files
           .sort((a, b) => b.mtime.localeCompare(a.mtime))
-          .map(file => (
-            <Grid item xl={2} lg={3} md={4} sm={6} xs={12} key={file.filename} data-testid='grid-inner'>
-              <Paper className={classes.paper} data-testid='paper'>
-                <ReactPlayer
-                  url={`${config.get('videoServer')}/${file.filename}`}
-                  width='100%'
-                  height='100%'
-                  controls
-                  className='ReactPlayer'
-                  data-testid='react-player'
-                />
-                <div className={classes.filename} data-testid='filename'>{file.filename}</div>
-                <div className={classes.filesize} data-testid='filesize'>{filesize(file.size)}</div>
-              </Paper>
-            </Grid>
-          ))}
+          .map(file =>
+            /^.+\.(avi|flv|wmv|mp4|mov|m4v)$/i.test(file.filename) ? (
+              <Grid item xl={2} lg={3} md={4} sm={6} xs={12} key={file.filename} data-testid='grid-inner'>
+                <Paper className={classes.paper} data-testid='paper'>
+                  <ReactPlayer
+                    url={`${config.get('videoServer')}/${file.filename}`}
+                    width='100%'
+                    height='100%'
+                    controls
+                    className='ReactPlayer'
+                    data-testid='react-player'
+                  />
+                  <div className={classes.filename} data-testid='filename'>
+                    {file.filename}
+                  </div>
+                  <div className={classes.filesize} data-testid='filesize'>
+                    {filesize(file.size)}
+                  </div>
+                </Paper>
+              </Grid>
+            ) : (
+              ''
+            )
+          )}
       </Grid>
     </div>
-  );
+  )
 }
